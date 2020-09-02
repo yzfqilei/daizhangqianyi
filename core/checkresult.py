e744a518dd590772078a2080f32df00e0dbb044c
shanchenxing1 <568212681@qq.com>
2020-09-02 10:58:26 +0800

    优化校验规则

--- core/checkresult.py
+++ core/checkresult.py
@@ -9,7 +9,7 @@ from common.logger import logger
 import json
 
 
-def check_results(r: Response, case_info):
+def check_codes_msg(r: Response, case_info):
     """检查运行结果"""
     with allure.step("校验返回响应码"):
         allure.attach(name='预期响应码', body=str(case_info['expectcode']))
@@ -23,5 +23,17 @@ def check_results(r: Response, case_info):
         rjson = json.loads(r.text)
         pytest.assume(case_info['expectresult']['code'] == rjson['code'])
         pytest.assume(case_info['expectresult']['msg'] == rjson['msg'])
-        pytest.assume(str(case_info['expectresult']['data']) in str(rjson['data']))
+        # pytest.assume(str(case_info['expectresult']['data']) in str(rjson['data']))
         logger.info("expect:" + str(case_info['expectresult']) + "," + "actual:" + str(r.text))
+
+
+def check_datas(r, case_info, listname=None):
+    rjson = json.loads(r.text)
+    adata = rjson['data']
+    case_info_data = case_info['expectresult']['data']
+    if listname:
+        for ss in listname:
+            pytest.assume(str(case_info_data[ss]) == str(adata[ss]))
+            logger.info("expectdata:" + str(case_info_data[ss]) + "," + "actualdata:" + str(adata[ss]))
+    else:
+        pytest.assume(str(case_info_data) == str(adata))
