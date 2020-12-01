f133033a6d9ab87aff0157331711beed9bd1f231
shanchenxing <52355572scx>
2020-12-01 19:13:39 +0800

    指定字段值新增触发及不触发场景

--- testcases/apis_test/scx_cases/assign_field_values_test.py
+++ testcases/apis_test/scx_cases/assign_field_values_test.py
@@ -105,3 +105,45 @@ class TestAssignFieldValues(object):
         # convert_json_to_yaml(r.text, yaml_path, mainkey)
         check_codes_msg(r, yamlvalue, mainkey)
         check_datas(r, yamlvalue)
+
+    @allure.story("指定字段值触发（开票申请新增 -- 指定值12）")
+    def test08(self):
+        """指定字段值触发（开票申请新增 -- 指定值12）"""
+        csurl, method, head, yamlvalue, yaml_path, mainkey = get_common_params("assign-field-values.yaml",
+                                                                               "指定字段值触发（开票申请新增 -- 指定值12）")
+        xz_r = a.request(csurl[0], method, json=yamlvalue['data'], headers=head)
+        _id = xz_r.json()['data']['_id']
+        cx_data = {"field": "", "order": "", "conditions": {}, "pagination": {"pageIndex": 1}, "complexConditions": [],
+                   "pageSize": 10, "pageIndex": 1}
+        # 查询数据
+        cx_r = a.request(csurl[1], method, json=cx_data, headers=head)
+        # convert_json_to_yaml(cx_r.text, yaml_path, mainkey)
+        check_codes_msg(cx_r, yamlvalue, mainkey)
+        check_datas(cx_r, yamlvalue)
+        #删除数据
+        sc_r = a.request(csurl[2] + _id ,'delete',headers=head)
+        if sc_r.status_code == 200:
+            print("开票申请删除数据成功")
+        else:
+            raise Exception("数据删除失败")
+
+    @allure.story("指定字段值不触发（开票申请新增 -- 指定值12）")
+    def test09(self):
+        """指定字段值不触发（开票申请新增 -- 指定值12）"""
+        csurl, method, head, yamlvalue, yaml_path, mainkey = get_common_params("assign-field-values.yaml",
+                                                                               "指定字段值不触发（开票申请新增 -- 指定值12）")
+        xz_r = a.request(csurl[0], method, json=yamlvalue['data'], headers=head)
+        _id = xz_r.json()['data']['_id']
+        cx_data = {"field": "", "order": "", "conditions": {}, "pagination": {"pageIndex": 1}, "complexConditions": [],
+                   "pageSize": 10, "pageIndex": 1}
+        # 查询数据
+        cx_r = a.request(csurl[1], method, json=cx_data, headers=head)
+        # convert_json_to_yaml(cx_r.text, yaml_path, mainkey)
+        # 删除数据
+        sc_r = a.request(csurl[2] + _id, 'delete', headers=head)
+        if sc_r.status_code == 200:
+            print("开票申请删除数据成功")
+        else:
+            raise Exception("数据删除失败")
+        check_codes_msg(cx_r, yamlvalue, mainkey)
+        check_datas(cx_r, yamlvalue)
