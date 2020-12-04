a653b4f9c2e251b95bf2556b73bbd528afee8058
qilei <qilei@yunzhangfang.com>
2020-12-04 11:52:06 +0800

    调试流程用例。

--- testcases/apis_test/ql_cases/test_process_manage.py
+++ testcases/apis_test/ql_cases/test_process_manage.py
@@ -38,6 +38,21 @@ class Test_process_manage():
         print('----------查询成功！')
         return id, procDefId
 
+    def get_process_data(self, name):
+        func_name = sys._getframe().f_code.co_name
+        data = {"procDefName": name, "pageIndex": 1, "pageSize": 10, "startApplyTime": None, "endApplyTime": None,
+                "type": 0}
+        csurl, method, headers, yamlvalue, yaml_path, mainkey = get_common_params('process_manage.yaml', func_name)
+        a = RestClient(rooturl)
+        res = a.request(url=csurl, method=method, json=data, headers=headers)
+        dict = json.loads(res.text)
+        data = dict['data']['list'][0]
+        print(type(data),data)
+        procInstId = data['procInstId']
+        print('----------查询成功！')
+        return procInstId
+
+
     @allure.story("创建审批流")
     def test001_create_approval_flow(self):
         func_name = sys._getframe().f_code.co_name
@@ -45,8 +60,10 @@ class Test_process_manage():
         data={"procDef":{"procDefId":None,"bpmnXml":"<definitions xmlns=\"http://www.omg.org/spec/BPMN/20100524/MODEL\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:flowable=\"http://flowable.org/bpmn\" xmlns:bpmndi=\"http://www.omg.org/spec/BPMN/20100524/DI\" xmlns:omgdc=\"http://www.omg.org/spec/DD/20100524/DC\" xmlns:omgdi=\"http://www.omg.org/spec/DD/20100524/DI\" typeLanguage=\"http://www.w3.org/2001/XMLSchema\" expressionLanguage=\"http://www.w3.org/1999/XPath\" targetNamespace=\"http://www.flowable.org/processdef\"><process id=\"flowable-1602297919597\" name=\"approval_flow\" isExecutable=\"true\"><extensionElements/><startEvent id=\"node-start\" name=\"开始\" flowable:initiator=\"starter\"></startEvent><endEvent id=\"node-end\" name=\"结束\"></endEvent><userTask id=\"review-252a859f\" name=\"review\" flowable:formKey=\"\" flowable:candidateGroups=\"u_756574317326995456\"><extensionElements></extensionElements></userTask><userTask id=\"input-97814ba8\" name=\"node\" flowable:formKey=\"\"><extensionElements></extensionElements></userTask><sequenceFlow id=\"line-b0c73353\" sourceRef=\"node-start\" targetRef=\"input-97814ba8\"></sequenceFlow><sequenceFlow id=\"line-406f5e6d\" sourceRef=\"input-97814ba8\" targetRef=\"review-252a859f\"></sequenceFlow><sequenceFlow id=\"line-7a5fc671\" sourceRef=\"review-252a859f\" targetRef=\"node-end\"></sequenceFlow></process><bpmndi:BPMNDiagram id=\"BPMNDiagram_flowable-1602297919597\"><bpmndi:BPMNPlane bpmnElement=\"flowable-1602297919597\" id=\"BPMNPlane_flowable-1602297919597\"><bpmndi:BPMNShape bpmnElement=\"review-252a859f\" id=\"BPMNShape_review-252a859f\"><omgdc:Bounds height=\"40\" width=\"60\" x=\"385.015625\" y=\"116.5\"></omgdc:Bounds></bpmndi:BPMNShape><bpmndi:BPMNShape bpmnElement=\"input-97814ba8\" id=\"BPMNShape_input-97814ba8\"><omgdc:Bounds height=\"35\" width=\"60\" x=\"225.515625\" y=\"95.5\"></omgdc:Bounds></bpmndi:BPMNShape><bpmndi:BPMNShape bpmnElement=\"node-start\" id=\"BPMNShape_node-start\"><omgdc:Bounds height=\"50\" width=\"50\" x=\"50\" y=\"50\"></omgdc:Bounds></bpmndi:BPMNShape><bpmndi:BPMNShape bpmnElement=\"node-end\" id=\"BPMNShape_node-end\"><omgdc:Bounds height=\"50\" width=\"50\" x=\"650\" y=\"480\"></omgdc:Bounds></bpmndi:BPMNShape><bpmndi:BPMNEdge bpmnElement=\"line-b0c73353\" id=\"BPMNEdge_line-b0c73353\"><omgdi:waypoint x=\"0\" y=\"0\"></omgdi:waypoint><omgdi:waypoint x=\"0\" y=\"0\"></omgdi:waypoint></bpmndi:BPMNEdge><bpmndi:BPMNEdge bpmnElement=\"line-406f5e6d\" id=\"BPMNEdge_line-406f5e6d\"><omgdi:waypoint x=\"0\" y=\"0\"></omgdi:waypoint><omgdi:waypoint x=\"0\" y=\"0\"></omgdi:waypoint></bpmndi:BPMNEdge><bpmndi:BPMNEdge bpmnElement=\"line-7a5fc671\" id=\"BPMNEdge_line-7a5fc671\"><omgdi:waypoint x=\"0\" y=\"0\"></omgdi:waypoint><omgdi:waypoint x=\"0\" y=\"0\"></omgdi:waypoint></bpmndi:BPMNEdge></bpmndi:BPMNPlane></bpmndi:BPMNDiagram></definitions>","formId":"756585727382392840","name":"approval_flow","ruleCyclePeriod":"","ruleCycleTime":"undefined undefined  * * ?","ruleType":0,"status":0,"type":0,"pushFlag":0},"procNodes":[{"assignee":"756574317326995456","assigneeType":6,"formId":"756585727382392840","name":"review","formLayoutCode":"kaipiaoshenpi","pushMsg":0,"taskDefKey":"review-252a859f","id":"","sequence":1},{"assignee":"","assigneeType":-1,"formId":"756585727382392840","name":"node","formLayoutCode":"","pushMsg":0,"taskDefKey":"input-97814ba8","id":"","sequence":0}],"procMsg":{"assignee":"756585370497454081","assigneeType":5}}
         a = RestClient(rooturl)
         res = a.request(url=csurl, method=method, json=data, headers=headers)
+        print(res.text)
         check_codes_msg(res, yamlvalue, mainkey)
         check_datas(res, yamlvalue)
+
 
     @allure.story("创建工作流")
     def test002_create_work_flow(self):
@@ -153,17 +170,17 @@ class Test_process_manage():
         pytest.assume('approval_flow' in res.text)
         pytest.assume('work_flow' in res.text)
 
-    @allure.story("过滤所有状态的流程")
-    def test011_screen_status_all(self):
-        func_name = sys._getframe().f_code.co_name
-        csurl, method, headers, yamlvalue, yaml_path, mainkey = get_common_params('process_manage.yaml', func_name)
-        a = RestClient(rooturl)
-        res = a.request(url=csurl, method=method, headers=headers)
-        check_codes_msg(res, yamlvalue, mainkey)
-        pytest.assume('approval_flow' in res.text)
-        pytest.assume('work_flow' in res.text)
-        pytest.assume('跟进记录' in res.text)
-        pytest.assume('开票申请' in res.text)
+    # @allure.story("过滤所有状态的流程")
+    # def test011_screen_status_all(self):
+    #     func_name = sys._getframe().f_code.co_name
+    #     csurl, method, headers, yamlvalue, yaml_path, mainkey = get_common_params('process_manage.yaml', func_name)
+    #     a = RestClient(rooturl)
+    #     res = a.request(url=csurl, method=method, headers=headers)
+    #     check_codes_msg(res, yamlvalue, mainkey)
+    #     pytest.assume('approval_flow' in res.text)
+    #     pytest.assume('work_flow' in res.text)
+    #     pytest.assume('跟进记录' in res.text)
+    #     pytest.assume('开票申请' in res.text)
 
 # 审批菜单中的相关用例
     # -------begin-------
@@ -212,4 +229,23 @@ class Test_process_manage():
         res = a.request(url=csurl, method=method, headers=headers)
         check_codes_msg(res, yamlvalue, mainkey)
         check_datas(res, yamlvalue)
+
+
+
+    def test014_delete_approval_flow_data(self):
+        procDefId = Test_process_manage.get_process_data(self,'approval_flow')
+        func_name = sys._getframe().f_code.co_name
+        csurl, method, headers, yamlvalue, yaml_path, mainkey = get_common_params('process_manage.yaml', func_name)
+        data = {"deleteReason":"","procInsId":[procDefId]}
+        a = RestClient(rooturl)
+        res = a.request(url=csurl, method=method, json= data, headers=headers)
+        check_codes_msg(res, yamlvalue, mainkey)
+        check_datas(res, yamlvalue)
+        #核实删除结果在查询下
+        try:
+            result = Test_process_manage.get_process_data(self, 'approval_flow')
+        except IndexError as e:
+            print('数据为空，删除成功！')
+        else:
+            print('删除失败！')
 
